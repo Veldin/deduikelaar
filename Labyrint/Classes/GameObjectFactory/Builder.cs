7aaedefcb83ee2a67e6dd63cc94e43cc4f486a8a
Alex Jonkman <alex.jonkman1@student.stenden.com>
2019-05-13 15:36:23 +0200

    Added the GameObjectFactory sub-system

---
+++ Labyrint/Classes/GameObjectFactory/Builder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameObjectFactory
+{
+    public class Builder
+    {
+        public Builder()
+        {
+
+        }
+
+        public void TransformGameObject(GameObject target, string wantToGet)
+        {
+            
+        }
+    }
+}
