e05bbb94a87523fb4097a14031c94b6536cf945c
99ro <robindeboer1999@gmail.com>
2019-05-13 15:41:49 +0200

    Added the LogSystem without UnitTests

---
+++ Log/ELogLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace LogSystem
+{
+    /// <summary>
+    /// This enum describes the importance of a logMessage
+    /// </summary>
+    public enum ELoglevel
+    {
+        Unknown,        // Use this when the loglevel is null or unknown
+        Debug,          // Use this to inform the progrommer
+        Info,           // Use this to inform the user
+        Warning,        // Use this if something went wrong but the Engine can keep running
+        Error           // Use this if something is wrong and the Engine breaks
+    };
+}
