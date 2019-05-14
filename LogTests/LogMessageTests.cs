5e8026ff85025775e69192d4e95fccc4d580ad4b
99ro <robindeboer1999@gmail.com>
2019-05-14 13:23:13 +0200

    Added UnitTests for the Log

---
+++ LogTests/LogMessageTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogSystem;
+
+namespace LogSystem.Tests
+{
+    [TestClass()]
+    public class LogMessageTests
+    {
+        [TestMethod()]
+        public void LogMessageTest()
+        {
+            LogMessage testMessage = new LogMessage("Hallo", ELoglevel.Debug, "14/05/2019 11:00:00,236", "LogMessageTest.cs", "GetMessageTest", 12);
+
+            // Test Getters
+            Assert.AreEqual(testMessage.GetMessage(), "Hallo");
+            Assert.AreEqual(testMessage.GetLogLevel(), ELoglevel.Debug);
+            Assert.AreEqual(testMessage.GetCreationTime(), "14/05/2019 11:00:00,236");
+            Assert.AreEqual(testMessage.GetFilePath(), "LogMessageTest.cs");
+            Assert.AreEqual(testMessage.GetMemberName(), "GetMessageTest");
+            Assert.AreEqual(testMessage.GetLineNumber(), 12);
+        }
+    }
+}
---
+++ LogTests/LogTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogSystem;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LogSystem.Tests
+{
+    [TestClass()]
+    public class LogTests
+    {
+        [TestMethod()]
+        public void GetLogMessagesTest()
+        {
+            List<LogMessage> logMessages = Log.GetLogMessages();
+            Assert.IsTrue(logMessages.Count == 0);
+            Log.Debug("Test");
+            logMessages = Log.GetLogMessages();
+            Assert.IsFalse(logMessages.Count == 0);
+        }
+
+        [TestMethod()]
+        public void MessageTest()
+        {
+            Log.Message("This is a test message", ELoglevel.Info);
+            LogMessage testMessage = Log.GetLogMessages().Last();
+            
+            // Test Getters
+            Assert.AreEqual(testMessage.GetMessage(), "This is a test message");
+            Assert.AreEqual(testMessage.GetLogLevel(), ELoglevel.Info);
+            Assert.IsNotNull(testMessage.GetCreationTime());
+            Assert.IsNotNull(testMessage.GetFilePath());
+            Assert.AreEqual(testMessage.GetMemberName(), "MessageTest");
+            Assert.AreEqual(testMessage.GetLineNumber(), 27);
+        }
+
+        [TestMethod()]
+        public void DebugTest()
+        {
+            Log.Debug("This is a test message");
+            LogMessage testMessage = Log.GetLogMessages().Last();
+
+            // Test Getters
+            Assert.AreEqual(testMessage.GetMessage(), "This is a test message");
+            Assert.AreEqual(testMessage.GetLogLevel(), ELoglevel.Debug);
+            Assert.IsNotNull(testMessage.GetCreationTime());
+            Assert.IsNotNull(testMessage.GetFilePath());
+            Assert.AreEqual(testMessage.GetMemberName(), "DebugTest");
+            Assert.AreEqual(testMessage.GetLineNumber(), 42);
+        }
+
+        [TestMethod()]
+        public void InfoTest()
+        {
+            Log.Info("This is a test message");
+            LogMessage testMessage = Log.GetLogMessages().Last();
+
+            // Test Getters
+            Assert.AreEqual(testMessage.GetMessage(), "This is a test message");
+            Assert.AreEqual(testMessage.GetLogLevel(), ELoglevel.Info);
+            Assert.IsNotNull(testMessage.GetCreationTime());
+            Assert.IsNotNull(testMessage.GetFilePath());
+            Assert.AreEqual(testMessage.GetMemberName(), "InfoTest");
+            Assert.AreEqual(testMessage.GetLineNumber(), 57);
+        }
+
+        [TestMethod()]
+        public void WarningTest()
+        {
+            Log.Warning("This is a test message");
+            LogMessage testMessage = Log.GetLogMessages().Last();
+
+            // Test Getters
+            Assert.AreEqual(testMessage.GetMessage(), "This is a test message");
+            Assert.AreEqual(testMessage.GetLogLevel(), ELoglevel.Warning);
+            Assert.IsNotNull(testMessage.GetCreationTime());
+            Assert.IsNotNull(testMessage.GetFilePath());
+            Assert.AreEqual(testMessage.GetMemberName(), "WarningTest");
+            Assert.AreEqual(testMessage.GetLineNumber(), 72);
+        }
+
+        [TestMethod()]
+        public void ErrorTest()
+        {
+            Log.Error("This is a test message");
+            LogMessage testMessage = Log.GetLogMessages().Last();
+
+            // Test Getters
+            Assert.AreEqual(testMessage.GetMessage(), "This is a test message");
+            Assert.AreEqual(testMessage.GetLogLevel(), ELoglevel.Error);
+            Assert.IsNotNull(testMessage.GetCreationTime());
+            Assert.IsNotNull(testMessage.GetFilePath());
+            Assert.AreEqual(testMessage.GetMemberName(), "ErrorTest");
+            Assert.AreEqual(testMessage.GetLineNumber(), 87);
+        }
+    }
+}
