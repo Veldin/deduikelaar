71a6cb9aea07b6d20fd84ae9b39bd7d6764c9ac5
Rowan Nijland <rowanijland@live.nl>
2019-05-15 15:06:38 +0200

    start input field

--- Dashboard/Dashboard/resources/js/components/CreateItem.js
+++ Dashboard/Dashboard/resources/js/components/CreateItem.js
@@ -4,30 +4,26 @@ import { Router, Route, Link } from 'react-router';
 class CreateItem extends Component {
     render() {
       return (
-      <div>
-        <h1>Maak een item aan</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col s6 inputFields" >
         <form>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <label>Titel:</label>
-                <input type="text" className="form-control" />
-              </div>
-            </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <label>Beschrijving:</label>
-                  <input type="text" className="form-control col-md-6" />
-                </div>
-              </div>
-            </div><br />
-            <div className="form-group">
-              <button className="btn btn-primary">Opslaan</button>
-            </div>
+        <div className="row">
+          <div class="input-field col s6">
+            <input id="last_name" type="text"></input>
+            <label for="last_name">Titel</label>
+          </div>
+          </div>
         </form>
-  </div>
+
+          </div>
+
+
+
+          <div className="col s6 inputFields">
+          </div>
+        </div>
+      </div>
       )
     }
 }
