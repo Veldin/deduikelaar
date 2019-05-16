7108c2df3a410c57493583b8fe6afb456221af8a
jaroen007 <jaroeneefting@hotmail.com>
2019-05-16 12:57:48 +0200

    navigation fixed

--- Dashboard/Dashboard/resources/js/components/Overview.js
+++ Dashboard/Dashboard/resources/js/components/Overview.js
@@ -6,12 +6,19 @@ class Overview extends Component {
     return (
       <div class="container">
         <div class="row">
-          <div class="col s3 navigationContainer">
-            <Link class="col s12 navigationItem" to="/overview">Overzicht</Link>
-            <Link class="col s12 navigationItem" to="/add-item">Item Toevoegen</Link>
-            <Link class="col s12 navigationItem" to="/see-example">Voorbeeld</Link>
+          <div class="col s2 navigationContainer">
+            <Link class="col s12 navigationItem" activeClassName="col s12 navigationItem active" to="/overview">
+              <span class="navigationItemText">Overzicht</span>
+            </Link>
+            <Link class="col s12 navigationItem" activeClassName="col s12 navigationItem active" to="/add-item">
+              <span class="navigationItemText">Item Toevoegen</span>
+            </Link>
+            <Link class="col s12 navigationItem" activeClassName="col s12 navigationItem active" to="/see-example">
+              <span class="navigationItemText">Voorbeeld</span>
+            </Link>
           </div>
-          <div class="col s9 overviewContainer">2</div>
+          <div class="col s1"></div>
+          <div class="col s9 contentContainer">2</div>
         </div>
       </div>
     )
