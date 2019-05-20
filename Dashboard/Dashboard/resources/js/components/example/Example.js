1b42f46b083d9366702d1f67f02f7e9346fc9658
Rowan Nijland <rowanijland@live.nl>
2019-05-20 16:10:26 +0200

    removed card

--- Dashboard/Dashboard/resources/js/components/example/Example.js
+++ Dashboard/Dashboard/resources/js/components/example/Example.js
@@ -3,12 +3,18 @@ import React from 'react';
 const example = (props) => {
     return (   
         <div className="col s12 background">
-            <div className="col s12 card">
+            <div className="col s12 exampleCard">
                 <div className="title">
                 </div>
                 <div className="content">
                 </div>
                 <div className="title">
+                </div>
+
+                <div className="feedback">
+                <div className="feedback">
+                </div>
+
                 </div>
 
             </div>
