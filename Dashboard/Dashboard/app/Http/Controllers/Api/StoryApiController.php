6c7cffecefff2204ade035dfd1a2eda302452d2b
alwintje <alwinkroesen@gmail.com>
2019-05-29 14:46:52 +0200

    Comments

--- Dashboard/Dashboard/app/Http/Controllers/Api/StoryApiController.php
+++ Dashboard/Dashboard/app/Http/Controllers/Api/StoryApiController.php
@@ -19,12 +19,21 @@ class StoryApiController extends Controller
 {
 
 
+    /**
+     * Page for the overview
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getOverview(){
 
         $data = [];
+
+        // Get all stories
         $stories = \App\Story::with('feedback')->get();
+
+        // Get all feedback items
         $feedbacks = \App\Feedback::with('feedbackItems')->get();
 
+        // Loop through all stories
         foreach ($stories as $story){
             $questions = [];
             foreach ($feedbacks as $feedback){
@@ -75,6 +84,11 @@ class StoryApiController extends Controller
         return  response()->json($data);
     }
 
+    /**
+     * Get all data of a single story
+     * @param $storyId
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getStory($storyId){
 
         $data = [];
@@ -96,8 +110,8 @@ class StoryApiController extends Controller
         // Set data to return
         $data['id'] = $story->id;
         $data['title'] = $story->title;
-        $data['icon'] = $story->title;
-        $data['description'] = $story->title;
+        $data['icon'] = $story->icon;
+        $data['description'] = $story->description;
         $data['texts'] = [];
 
         // Add story items
@@ -126,11 +140,19 @@ class StoryApiController extends Controller
         return response()->json($data);
     }
 
+    /**
+     * Get a list of active stories
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Throwable
+     */
     public function getStories(){
 
         $data = [];
+
+        // Get all active stories
         $stories = Story::with('storyItems')->where('active', 1)->get();
 
+        // Set the correct data
         foreach ($stories as $story){
             $data[] = [
                 'storyId' => $story->id,
@@ -141,19 +163,28 @@ class StoryApiController extends Controller
         return response()->json($data);
     }
 
+    /**
+     * Create a new story
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function newStory(Request $request){
 
+        // Get the post variables
         $title = $request->get('title');
         $icon = $request->get('icon');
         $description = $request->get('description');
         $texts = $request->get('texts');
         $files = $request->file('files');
 
+
+        // Validate
         $validation = Validator::make($request->all(), [
             'title' => 'required',
             'icon' => 'required'
         ]);
 
+        // When validation fails, return error
         if ($validation->fails()) {
             return response()->json([
                 'response' => 'failed',
@@ -161,18 +192,22 @@ class StoryApiController extends Controller
             ]);
         }
 
+        // Set story data
         $storyData = [
             'title' => $title,
             'icon' => $icon
         ];
 
+        // When there is a description, add it.
         if(strlen($description) > 0){
             $storyData['description'] = $description;
         }
 
 
+        // Create story
         $story = Story::create($storyData);
 
+        // Add texts
         if($texts) {
             if(!is_array($texts)){
                 $texts = [$texts];
@@ -184,21 +219,29 @@ class StoryApiController extends Controller
                 ]);
             }
         }
+        // Add files
         if($files){
 
             foreach ($files as $file) {
+
+                // Get file data
                 $f = $file;
                 $extension = $f->getClientOriginalExtension();
                 $fn = $f->getClientOriginalName();
 
+                // Create a new filename
                 $filename = Carbon::now()->format('Ymdhis') . rand(11111111, 99999999) . '.' . $extension;
+
+                // Save file
                 $file->storeAs("/uploads/story/", $filename, 'local');
 
+                // Create storyItem for this file
                 $storyItem = StoryItem::create([
                     'text' => null,
                     'storyId' => $story->id
                 ]);
 
+                // Create file
                 $file = File::create([
                     'fileName' => $filename,
                     'realName' => $fn,
@@ -207,6 +250,8 @@ class StoryApiController extends Controller
                     'path' => 'app/uploads/story/',
                     'storyItemId' => $storyItem->id
                 ]);
+
+                // Set story item text if the file is an docx file
                 $text = $this->convertDocxFile($file);
                 if($text != null){
                     $storyItem->update(['text' => $text]);
@@ -214,6 +259,7 @@ class StoryApiController extends Controller
             }
         }
 
+        // Return success
         return response()->json([
             'response' => 'success',
             'storyId' => $story->id
@@ -262,55 +308,90 @@ class StoryApiController extends Controller
     }
 
 
-
+    /**
+     * Delete a story
+     * @param $storyId
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
     public function deleteStory($storyId){
+
+        // Get story
         $story = Story::with('storyItems', 'storyItems.file')->find($storyId);
-        if($story){
-            foreach ($story->storyItems as $storyItem){
-                if(isset($storyItem->file)){
-                    $file = storage_path($storyItem->file->path.$storyItem->file->fileName);
-                    if(file_exists($file)){
-                        unlink($file);
-                    }
-                }
-            }
-            $story->delete();
+
+        //
+        if(!$story){
+
             return response()->json([
-                'response' => 'success'
+                'response' => 'failed',
+                'errors' => [
+                    'Story not found'
+                ]
             ]);
         }
+
+        // Remove files of the story
+        foreach ($story->storyItems as $storyItem){
+            if(isset($storyItem->file)){
+                $file = storage_path($storyItem->file->path.$storyItem->file->fileName);
+                if(file_exists($file)){
+                    unlink($file);
+                }
+            }
+        }
+
+        // Delete story
+        $story->delete();
+
+
         return response()->json([
-            'response' => 'failed'
+            'response' => 'success'
         ]);
     }
 
 
+    /**
+     * Convert a docx file to text
+     * @param File $file
+     * @return bool|string|null
+     */
     public function convertDocxFile(File $file){
 
+        // Check if it is a docx file
         if($file->extension != 'docx') return null;
         $content = '';
 
+        // Open file as a zip file
         $zip = zip_open(storage_path($file->path.$file->fileName));
 
-        if (!$zip || is_numeric($zip)) return false;
+        // Check if it is an zip file
+        if (!$zip || is_numeric($zip)) return null;
         $media = [];
         $relations = "";
+
+        // Loop through files
         while ($zip_entry = zip_read($zip)) {
 
             if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+
+            // Set relations
             if(substr(zip_entry_name($zip_entry), 0, 11) == "word/_rels/"){
                 $relations = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
             }
+
+            // Set media
             if(substr(zip_entry_name($zip_entry), 0,11) == "word/media/"){
 
                 $media[substr(zip_entry_name($zip_entry), 5)] = [
                     'size' => zip_entry_filesize($zip_entry),
                     'data' => base64_encode(zip_entry_read($zip_entry, zip_entry_filesize($zip_entry)))
                 ];
-//                var_dump(zip_entry_name($zip_entry));
             }
+
+            // Ignore all other files, except for the actual document
             if (zip_entry_name($zip_entry) != "word/document.xml") continue;
 
+            // Set the content
             $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
 
             zip_entry_close($zip_entry);
@@ -318,21 +399,26 @@ class StoryApiController extends Controller
 
         zip_close($zip);
 
+
+        // Read relations as XML
         $xmlRel = simplexml_load_string($relations);
 
-        $relations = [];
+        // Change media array to key -> value where key is the id from relations file
         foreach ($xmlRel->Relationship as $relation) {
             $attributes = $relation->attributes();
-//            var_dump((string)$attributes->Type);
             if(strpos((string) $attributes->Type, 'image') !== false){
                 $media[(string)$attributes->Id] = $media[(string)$attributes->Target];
                 unset($media[(string)$attributes->Target]);
             }
         }
 
+        // Read document as XML
         $xml = simplexml_load_string($content,null, 0, 'w', true);
+
+        // find all namespaces
         preg_match_all('/xmlns:(.*?)="(.*?)"/i', $content, $matches);
 
+        // Add all the namespaces to the xml reader
         $namespaces = [];
         if(count($matches) > 2){
             foreach ($matches[1] as $i => $v){
@@ -343,24 +429,34 @@ class StoryApiController extends Controller
             }
         }
 
+        // Document body
         $body = $xml->body;
 
         $content = "";
         $mediaIndex = 0;
+        // Loop through each tag that is directly in the body tag
         foreach($body[0] as $key => $value){
+
+            // Open html paragraph
             $content .= "<p>";
             if($key == "p"){
+
+                // Set text
                 foreach ($value->r as $kkey => $vvalue) {
                     $content .= (string)$vvalue->t;
                 }
 
+                // Check if there are images
                 $drawing = $this->findDrawing($value);
                 if($drawing){
+                    // Find image id
                     $search = $drawing->xpath("//*[local-name()='blip']");
                     if(isset($search[$mediaIndex])){
                         if(isset($search[$mediaIndex]->attributes('r', true)[0])){
                             $id = (string) $search[$mediaIndex]->attributes('r', true)[0];
                             if(isset($media[$id])){
+                                // image id found
+                                // Add image tag with base64 source
                                 $content .= "<img src='data:image/png;base64, ".$media[$id]['data']."'/>";
                             }
                             $mediaIndex++;
@@ -368,6 +464,8 @@ class StoryApiController extends Controller
                     }
                 }
             }
+
+            // End html paragraph
             $content .= "</p>";
         }
 
@@ -375,8 +473,14 @@ class StoryApiController extends Controller
 
     }
 
+    /**
+     * Find drawing tag for image
+     * @param $v \SimpleXMLElement
+     * @return \SimpleXMLElement|null
+     */
     private function findDrawing($v){
 
+        // Find drawing tag
         foreach ($v->r as $l => $w) {
             if(isset($w->drawing)){
                 return $w->drawing;
