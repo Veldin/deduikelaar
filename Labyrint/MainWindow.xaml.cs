7312426117666281b02919c37a5a659c105c722f
Veldin <amr.jonkman@gmail.com>
2019-07-02 16:03:47 +0200

    Added Release to git, also added an icon to the exe

--- Labyrint/MainWindow.xaml.cs
+++ Labyrint/MainWindow.xaml.cs
@@ -131,16 +131,16 @@ namespace Labyrint
             }
 
             //Set the windowState.
-            switch (SettingsFacade.Get("WindowState", "Normal", "Dictates the window state [Normal || Maximized || Minimized]"))
+            switch (SettingsFacade.Get("WindowState", "Maximized", "Dictates the window state [Maximized || Maximized || Normal]"))
             {
-                case "Maximized":
-                    WindowState = WindowState.Maximized;
+                case "Normal":
+                    WindowState = WindowState.Normal;
                     break;
                 case "Minimized":
                     WindowState = WindowState.Minimized;
                     break;
                 default:
-                    WindowState = WindowState.Normal;
+                    WindowState = WindowState.Maximized;
                     break;
             }
 
@@ -205,8 +205,11 @@ namespace Labyrint
                     browser.Margin = margin;
                 }));
 
+                //The file not found error in html.
+                string fileNorAPIfoundHTML = "<!doctype html><html lang='{{ app()->getLocale() }}'> <head> <meta charset='utf-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1'> <title>Error</title> <style> /**{*/ /*margin: 0;*/ /*padding: 0;*/ /*}*/ *{ max-width: 100%; } html, body{ position: absolute; width: 100%; height: 100%; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif; font-size: @if(isset($_GET['overview'])) 0.5rem @else 1rem @endif; background-color: #aa9479; margin: 0; padding: 0; overflow: auto; transform-origin: center center; } html{ overflow: hidden; } .title{ font-weight: 400; font-size: 1.8rem; margin-bottom: @if(isset($_GET['overview'])) 20px @else 60px @endif; width: 70%; } audio, video{ width: 100%; max-width: 100%; } img{ max-width: 100%; } .description,.feedback{ font-weight: normal; font-size: 1.3rem; }.feedback{display: block;width: 100%;text-align: center;} </style> </head> <body oncontextmenu='return false;'> <header> <h1 class='title'>Error</h1> </header> <div class='description'><p>Er kan geen verbinding met de server gemaakt worden. Tevens zijn er geen gecachte bestanden.</p><p>Zorg dat er informatiestukken op de server staan en maak vervolgens een verbinding met de server.</p> </div> </body></html>";
+
                 // Navigate the browser to the html page with the error message and set it on visible
-                browser.NavigateToString(FileReaderWriterFacade.ReadFile(FileReaderWriterFacade.GetProjectDirectoryPath() + "\\Assets\\MissingFileError.html"));
+                browser.NavigateToString(fileNorAPIfoundHTML);
                 browser.Visibility = Visibility.Visible;
 
                 // Create a button for the popup and add it to the gameObjects list
